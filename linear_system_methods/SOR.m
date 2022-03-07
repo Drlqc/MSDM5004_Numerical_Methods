d2230151e4b4e9d5ed90b4dc033866d2cf9998b2
Liqichao <92249448+Drlqc@users.noreply.github.com>
2022-03-07 14:16:21 +0800

    Add files via upload

---
+++ linear_system_methods/SOR.m
@@ -0,0 +1,28 @@
+%MSDM5004_Homework_02_3(2)SOR method
+syms x1 x2 x3 x4
+A = [4,1,-1,1;1,4,-1,-1;-1,-1,5,1;1,-1,1,3];
+b = [-3;-2;5;2];
+w = 1.2;
+x0 = [0;0;0;0];
+x = [x1;x2;x3;x4];
+x = x0;
+time = 0;
+while 1
+    time = time+1;
+    pre = x;
+    for i = 1:4
+        m = 0;
+        n = 0;
+        for j = 1:(i-1)
+            m = m-A(i,j)*x(j);
+        end
+        for j = (i+1):4
+            n = n-A(i,j)*x(j);
+        end
+        x(i) = (1-w)*x(i) + w/A(i,i)*(m+n+b(i));
+    end
+    aft = x;
+    if norm(aft-pre,inf) <= 10^(-3)
+        break
+    end
+end
